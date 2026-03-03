bc0a6079e0bc8376b72b71f2bf0b152defb2e6dd
Daniel.Solomon <daniel.solomon@fin.gc.ca>
2026-03-03 09:25:17 -0500

    let's try this again

--- Program.cs
+++ Program.cs
@@ -5,7 +5,21 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+
+//
+builder.Services.AddAuthentication();
+builder.Services.AddAuthorization();
+//
+
+
 var app = builder.Build();
+
+
+//
+app.UseAuthentication();
+app.UseAuthorization();
+//
+
 
 // For production scenarios, consider keeping Swagger configurations behind the environment check
 // if (app.Environment.IsDevelopment())
@@ -73,6 +87,52 @@ app.MapPost("/Person", (Person person) => {
 })
 .WithName("CreatePerson")
 .WithOpenApi();
+
+
+
+//
+app.MapGet("/userInfo", (HttpContext context) =>
+{
+    var user = context.User;
+
+    if (!user.Identity?.IsAuthenticated ?? true)
+        return Results.Unauthorized();
+
+    var response = new
+    {
+        Name = user.Identity.Name,
+        Claims = user.Claims.Select(c => new
+        {
+            Type = c.Type,
+            Value = c.Value
+        })
+    };
+
+    return Results.Json(response);
+});
+//.RequireAuthorization();
+//
+app.MapGet("/debug", (HttpContext ctx) =>
+{
+    var headers = ctx.Request.Headers
+        .Select(h => new { h.Key, Value = h.Value.ToString() });
+
+    return Results.Json(headers);
+});
+//
+app.MapGet("/whoami", (HttpContext ctx) =>
+{
+    return Results.Json(new
+    {
+        IsAuthenticated = ctx.User.Identity?.IsAuthenticated,
+        AuthType = ctx.User.Identity?.AuthenticationType,
+        ClaimsCount = ctx.User.Claims.Count()
+    });
+});
+//
+
+
+app.MapGet("/", () => "Hello World! Go to /swagger or /userInfo");
 
 app.MapGet("/", () => "Hello World! Go to /swagger");
 
